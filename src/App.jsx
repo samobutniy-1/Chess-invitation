679f5991c15396b1440dae9e0a179b41ecb9b5b0
webOwl <nikitaterentev916@gmail.com>
2026-03-19 16:43:54 +0100

    feat: create Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import { Header } from "./components/header/Header"
 
 
 function App() {
   
 
   return (
-   <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <>
+    <Header />
+    </>
   )
 }
 
