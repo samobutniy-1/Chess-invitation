77c6a83e7e8df2cbb2776b0a470da94f75aac846
webOwl <nikitaterentev916@gmail.com>
2026-03-31 13:42:54 +0200

    style: make Header responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,27 +15,27 @@ export function Header() {
 
       <div className="relative z-10 flex flex-col justify-between h-full p-5 text-center">
         <div className="flex flex-col items-center text-gray-200">
-          <h1 className="font-black uppercase text-[40px] w-full mb-12 mt-18">
+          <h1 className="font-black uppercase text-[clamp(40px,5vw,84px)] w-full mb-12 mt-18 ">
             Make your first move
           </h1>
 
           <a
             href="/"
-            className="font-montserrat font-black uppercase text-[32px] border-2 rounded-2xl max-w-xs bg-[#1E1E1E]/75 py-5 px-9 hover:text-black hover:border-black hover:bg-white/75 transition duration-300"
+            className="font-montserrat font-black uppercase text-[clamp(32px,5vw,40px)] border-2 rounded-2xl max-w-xs bg-[#1E1E1E]/75 py-5 px-9 hover:text-black hover:border-black hover:bg-white/75 transition duration-300  xl:max-w-sm "
           >
             Sign up
           </a>
         </div>
 
         <div className="flex items-center justify-between font-black text-gray-200 uppercase gap-2 text-2xl mb-1">
-          <span className="bg-[#1a1a1a]/70 py-3.5 w-full rounded-2xl">
+          <span className="bg-[#1a1a1a]/70 py-3.5 w-full rounded-2xl  text-[clamp(24px,5vw,50px)]">
             Play
           </span>
-          <span className="bg-[#1a1a1a]/70 py-3.5 w-full rounded-2xl">
+          <span className="bg-[#1a1a1a]/70 py-3.5 w-full rounded-2xl  text-[clamp(24px,5vw,50px)]">
             Think
           </span>
-          <span className="bg-[#1a1a1a]/70 py-3.5 w-full rounded-2xl">
-            Think
+          <span className="bg-[#1a1a1a]/70 py-3.5 w-full rounded-2xl  text-[clamp(24px,5vw,50px)]">
+            Win
           </span>
         </div>
       </div>
